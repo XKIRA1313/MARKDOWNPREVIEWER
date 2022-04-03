ca04155a82e284681f40ac0dc663faa2cdeccc2b
XKIRA1313 <31453888+XKIRA1313@users.noreply.github.com>
2022-04-02 22:17:40 -0700

    Made the finished App

--- index.js
+++ index.js
@@ -1,44 +1,75 @@
+let initalState = `# Kira Martorano's
+# Markdown Previewer
+[Learn more about Kira Martorano here](http://xkira1313.com)
+<dl>
+<dt>HTML quick tips</dt>
+<dd>Things you may not know</dd>
+
+<dt>Did you know that when using html</dt>
+<dd>sometimes you may want a section of inline cord like so?</dd>
+</dl>
+
+## Thanks so much for the support
+
+- don't forget
+- to code
+- as much as you can!
+
+\`Here is an example of inline code\`
+
+\`\`\`
+function exampleOfMultiLineCode() {
+
+var a = 10;
+for (let i = 0; i < a, i++)
+}
+\`\`\`
+
+[![Kira Martorano's Logo:](https://i1.sndcdn.com/avatars-000627434832-w6v6fq-t500x500.jpg)]
+
+> Blockquotes are very handy in email to emulate reply text.
+> This line is part of the same quote.
+
+**Here is some bolded text**`
+
 marked.setOptions({
-    breaks: true
-})
+    breaks: true,
+});
 
 const renderer = new marked.Renderer();
 
 function App() {
-    const [text, setText] = React.useState("");
+    const [text, setText] = React.useState(initalState);
 
-    render() {
-        const { text } = this.state;
-    }
-
-    return(
+    return (
         <div className="text-center px-4">
             <h1 className="p-4">My Markdown Previewer</h1>
+            <h3 className="mt-3 inputheading">Input</h3>
             <textarea
                 name="text"
-                id="text"
+                id="editor"
                 rows="10"
                 value={text}
                 onChange={(e) => setText(e.target.value)}
                 className="textarea"    
-            ></textarea>
-            <h3 className="mt-3">Output</h3>
+            >
+                
+            </textarea>
+            <h3 className="mt-3 outputheading">Output</h3>
             <Preview markdown={text} />
         </div>
-    )
+    );
 }
 
-const markdown = marked(text);
-
-function Preview({ markdown }){
+function Preview({ markdown }) {
     return (
         <div
             dangerouslySetInnerHTML={{
-            __html: markdown}}
+                __html: marked(markdown, { renderer: renderer }),
+            }}
             id="preview"
-            className="text-center"
         ></div>
-    )
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
